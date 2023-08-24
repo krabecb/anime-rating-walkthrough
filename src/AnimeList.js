c85ec93c81e6db04f4315e12b6d5bc7791db3489
krabecb <briankrabec@gmail.com>
2023-08-24 09:02:28 -0500

    Completed walkthrough

--- src/AnimeList.js
+++ src/AnimeList.js
@@ -3,12 +3,24 @@ import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 
-export default function AnimeList() {
+export default function AnimeList(props) {
     // console.log(props)
 
     return (
         <div className="AnimeList">
-
+            <h3>{props.anime.name}</h3>
+            <img src={props.anime.imgSrc} />
+            <Box
+                sx={{
+                    '& > legend': { mt: 2 },
+                }}
+            />
+                <Typography component="legend">Controlled</Typography>
+                <Rating
+                    name="simple-controlled"
+                    value={props.anime.rating}
+                    onChange={(e) => {props.handleRating(e, props.anime.name)}}
+                />
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AnimeList from './AnimeList'
 import { useState} from 'react'
 
 function App() {
-  //PROPS, STATE, HOOK, EVENTS
+  //PROPS, STATE✅, HOOK✅, EVENTS
   const [animes, setAnimes] = useState([
     {
       name: "Demon Slayer",
@@ -21,12 +21,37 @@ function App() {
       rating: null
     },
   ])
+  
+  //The reason I have handleRating right on the highest level is so that I can use the setAnimes method in the useState hook above
+
+  function handleRating(e, title) {
+    // e.preventDefault()
+    // console.log(parseInt(e.target.defaultValue))
+    // console.log(title)
+    // updatedAnimes will reflect the user's star rating input. This is an array of all our data, but only updates the rating system changed.
+    const updatedAnimes = animes.map(currentIteration => {
+      //If the current show title matches the title the user interacted with, then update the rating
+      if (currentIteration.name === title) {
+        currentIteration.rating = parseInt(e.target.defaultValue)
+      }
+      return currentIteration
+    })
+    //Update our useState hook with the updated rating and all our other previous data
+    //SetAnimes is async -> when the process of setting the updated array, the component will rerender
+    setAnimes(updatedAnimes)
+  }
+
+  const animesListed = animes.map((eachAnime, idx) => (
+    //eachAnime has these properties available in our AnimeList component: name, imgSrc, and rating
+    // First iteration example: AnimeList component has the key-values: name: "Demon Slayer", imgSrc: "https://...", rating: null
+    <AnimeList key={idx} anime={eachAnime} handleRating={handleRating} setAnimes={setAnimes}/>
+  ))
 
   return (
     <div className="App">
       <h1>Anime Ratings</h1>
       <div className="anime-container">
-        TBD
+        {animesListed}
       </div>
     </div>
   );
