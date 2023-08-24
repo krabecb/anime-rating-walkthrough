5f3411200ea4608e16f83d20cb2e4428fda7f329
krabecb <briankrabec@gmail.com>
2023-08-23 21:05:28 -0500

    Starter

--- src/AnimeList.js
+++ src/AnimeList.js
@@ -3,24 +3,12 @@ import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 
-export default function AnimeList(props) {
+export default function AnimeList() {
     // console.log(props)
 
     return (
         <div className="AnimeList">
-            <h3>{props.anime.name}</h3>
-            <img src={props.anime.imgSrc}/>
-            <Box
-                sx={{
-                    '& > legend': { mt: 2 },
-                }}
-            />
-                <Typography component="legend">Rating:</Typography>
-                <Rating
-                    name="simple-controlled"
-                    value={props.anime.rating}
-                    onChange={(e) => {props.handleRating(e, props.anime.name)}}
-                />
+
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import AnimeList from './AnimeList'
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 
 function App() {
-  //PROPS✅, STATE✅, HOOK✅, EVENTS
+  //PROPS, STATE, HOOK, EVENTS
   const [animes, setAnimes] = useState([
     {
       name: "Demon Slayer",
@@ -22,32 +22,11 @@ function App() {
     },
   ])
 
-  useEffect(() => {
-
-  }, [animes])
-
-  function handleRating(e, title) {
-    e.preventDefault()
-    // console.log(parseInt(e.target.defaultValue), title)
-    // PROVIDE EXPLANATION OF MAP
-    const updatedAnimes = animes.map(anime => {
-      if (anime.name === title) {
-        anime.rating = parseInt(e.target.defaultValue)
-      }
-      return anime
-    })
-    setAnimes(updatedAnimes)
-  }
-
-  const animesListed = animes.map((eachAnime, idx) => (
-    <AnimeList key={idx} anime={eachAnime} handleRating={handleRating}/>
-  ))
-
   return (
     <div className="App">
       <h1>Anime Ratings</h1>
       <div className="anime-container">
-        { animesListed }
+        TBD
       </div>
     </div>
   );
